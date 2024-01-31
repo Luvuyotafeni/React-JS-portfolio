3cc1b97b71899b7f2a017c01920524740ee2fe54
luvuyo <luvuyotafeni012@gmail.com>
2024-01-31 19:34:30 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from "./component/Head/Header"
 import "./App.css"
 import Home from "./component/Hero/home"
+import About from './component/About/About'
 
 const App = () => {
   return (
     <>
       <Header />
       <Home />
+      <About/>
     </>
   )
   
