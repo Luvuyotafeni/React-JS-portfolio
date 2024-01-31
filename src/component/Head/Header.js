edb1de17d070a6667ee0ed19546bccc573ca1de5
luvuyo <luvuyotafeni012@gmail.com>
2024-01-31 20:35:00 +0200

    resume apis

--- src/component/Head/Header.js
+++ src/component/Head/Header.js
@@ -22,7 +22,7 @@ const Header = () => {
                     {/*<ul className='link f_flex uppercase'>*/}
                     <ul className={Mobile ? "nav-links-mobile" : "link f_flex uppercase"} onClick={()=> setMobile(false)}>
                         <li><a href='#home'> Home</a></li>
-                        <li><a href='#About'> About</a></li>
+                        <li><a href='#about'> About</a></li>
                         <li><a href='#skills'> Skills</a></li>
                         <li><a href='#projects'> Project</a></li>
                         <li><a href='#contact'> Contact Me</a></li>
