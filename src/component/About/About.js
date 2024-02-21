a6ee997da9af4d2145c1036b09dc161c24d6d9ab
luvuyo <luvuyotafeni012@gmail.com>
2024-02-21 02:14:22 +0200

    adding about

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -1,47 +1,83 @@
-import React from 'react'
-import "./About.css"
-import Card from "./card"
-import ResumeApi from "./ResumeApi"
+import React, { useState } from 'react';
+import './About.css';
+import Card from './card';
+import ResumeApi from './ResumeApi';
 
 const About = () => {
+  const [showEducation, setShowEducation] = useState(false);
+  const [showExperience, setShowExperience] = useState(true); // Default to show experience
+
   return (
     <>
-    <section className='About' id='about'>
+      <section className='About' id='about'>
         <div className='container-top'>
-            <div className='heading text-center'>
-                <h2>ABOUT</h2>
+          <div className='heading text-center'>
+            <h2>ABOUT</h2>
+          </div>
+          <div className='content-section mtop d_flex'>
+            <div className='left'>
+              <div className='heading'>
+                <button
+                  className='toggle-button'
+                  onClick={() => {
+                    setShowExperience(true);
+                    setShowEducation(false);
+                  }}
+                >
+                  Experience
+                </button>
+              </div>
+              <div className='content'>
+                {showExperience &&
+                  ResumeApi.map((val, id) => {
+                    if (val.category === 'experience') {
+                      return (
+                        <Card
+                          key={id}
+                          title={val.title}
+                          year={val.year}
+                          rate={val.rate}
+                          desc={val.desc}
+                        />
+                      );
+                    }
+                  })}
+              </div>
             </div>
-            <div className='content-section mtop d_flex'>
-                <div className='left'>
-                    <div className='heading'>
-                        <h4> Experience</h4>
-                    </div>
-                    <div className='content'>
-                        {ResumeApi.map((val,id) =>{
-                            if(val.category=== "experience"){
-                                return <Card key={id} title={val.title} year={val.year} rate={val.rate} desc={val.desc}/>
-                            }
-                        })}
-                    </div>
-                </div>
-                <div className='left'>
-                    <div className='heading'>
-                        <h4> Education</h4>
-                    </div>
-                    <div className='content'>
-                        {ResumeApi.map((val,id) =>{
-                            if(val.category=== "education"){
-                                return <Card key={id} title={val.title} year={val.year} rate={val.rate} desc={val.desc}/>
-                            }
-                        })}
-                    </div>
-                </div>
+            <div className='left'>
+              <div className='heading'>
+                <button
+                  className='toggle-button'
+                  onClick={() => {
+                    setShowEducation(true);
+                    setShowExperience(false);
+                  }}
+                >
+                  Education
+                </button>
+              </div>
+              <div className='content'>
+                {showEducation &&
+                  ResumeApi.map((val, id) => {
+                    if (val.category === 'education') {
+                      return (
+                        <Card
+                          key={id}
+                          title={val.title}
+                          year={val.year}
+                          rate={val.rate}
+                          desc={val.desc}
+                        />
+                      );
+                    }
+                  })}
+              </div>
             </div>
+          </div>
         </div>
-    </section>
-    
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
