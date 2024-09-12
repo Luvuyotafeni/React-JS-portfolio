40e363fa1ce12032f0c6e2f63aea2e51a6d213ed
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-09-12 10:15:14 +0200

    card icons

--- src/component/Skills/Skills.js
+++ src/component/Skills/Skills.js
@@ -16,21 +16,25 @@ const Skills = () => {
               <div className='col-md-3'>
                 <div className='card'>
                   <h5>Mobile App Development</h5>
+                  <p className='icon'><i class='bx bxs-mobile'></i></p>
                 </div>
               </div>
               <div className='col-md-3'>
                 <div className='card'>
                   <h5>Web Development</h5>
+                  <p className='icon'><i class='bx bx-globe'></i></p>
                 </div>
               </div>
               <div className='col-md-3'>
                 <div className='card'>
                   <h5>UX Development</h5>
+                  <p className='icon'><i class='bx bxl-sketch'></i></p>
                 </div>
               </div>
               <div className='col-md-3'>
                 <div className='card'>
                   <h5>Databases</h5>
+                  <p className='icon'><i class='bx bxs-data'></i></p>
                 </div>
               </div>
             </div>
